ca112a192a0ea5decf0eab9139cb6313b386c0f2
Miska Mäkelä <miska.makela@valagroup.com>
2020-05-04 21:23:25 +0300

    frontend setup

---
+++ README.md
@@ -0,0 +1,1 @@
+https://secure-wave-43930.herokuapp.com/api/persons
--- index.js
+++ index.js
@@ -38,6 +38,7 @@ morgan.token('body', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 app.get('/info', (request, response) => {
   const amount = persons.length
