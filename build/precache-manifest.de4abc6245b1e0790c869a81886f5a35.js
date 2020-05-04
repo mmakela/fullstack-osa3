ecaac1f6be06ad151ae9ac84fee714496903cc1b
Miska Mäkelä <miska.makela@valagroup.com>
2020-05-04 21:35:34 +0300

    script fix and uibuild

---
+++ build/precache-manifest.de4abc6245b1e0790c869a81886f5a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482d28a04dbb54cab62a41b0463cf3ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c28f5586565fbcb3ea",
+    "url": "/static/css/main.9edf4b42.chunk.css"
+  },
+  {
+    "revision": "d9189b8ec77a982c425a",
+    "url": "/static/js/2.d92478fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d92478fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c28f5586565fbcb3ea",
+    "url": "/static/js/main.090fb628.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
