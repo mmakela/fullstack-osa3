87cd843e92c2afd6d58e6ffeb1ea78f24fd66385
Miska Mäkelä <miska.makela@valagroup.com>
2020-05-04 13:41:14 +0300

    3.8 completed

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+morgan.token('body', (req) => {
+  if (req.method !== 'POST') {
+    return ' '
+  } 
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.json())
+
+app.get('/info', (request, response) => {
+  const amount = persons.length
+  const now = Date()
+  
+  response.send(`<p>Phonebook has info for ${amount} people</p><p>${now}</p>`)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = {...request.body, id: getRandomInt(10, 1000)}
+  if (!person.name) {
+    response.status(400).json({ error: 'name puuttuu' })
+    return
+  }
+  if (!person.number) {
+    response.status(400).json({ error: 'number puuttuu' })
+    return
+  }
+  if (persons.some(item => item.name === person.name)) {
+    response.status(400).json({ error: 'name must be unique' })
+    return
+  }
+  persons = persons.concat(person)
+  response.status(201).json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
